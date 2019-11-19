468958e5be018f4af016d3b31796c594642e759e
Stephen Smith <ssmith37651@gmail.com>
2019-11-19 09:16:13 -0500

    Initial Commit

---
+++ MyNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyNotes
+//
+//  Created by Smith, Stephen Christopher on 11/19/19.
+//  Copyright © 2019 Smith, Stephen Christopher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
